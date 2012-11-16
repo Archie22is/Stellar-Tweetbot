99e152975738f9574964c00f43acc3772b3615dc
Richard Eriksson <richard@justagwailo.com>
2012-11-15 21:19:23 -0800

    Whitespace.

--- index.php
+++ index.php
@@ -11,56 +11,56 @@ require_once $twitter_oauth_path;
 
 // This function grabs the last tweets we cached so we don't try to tweet them again... saves API hits
 function getOldIDs () {
-	global $cached_file_path;
-	$handle = fopen($cached_file_path, 'r');
-	$contents = fread($handle, filesize($cached_file_path));
-	fclose($handle);
+  global $cached_file_path;
+  $handle = fopen($cached_file_path, 'r');
+  $contents = fread($handle, filesize($cached_file_path));
+  fclose($handle);
 
-	$oldxml = simplexml_load_string($contents);
+  $oldxml = simplexml_load_string($contents);
 
-	$oldIDs = array();
+  $oldIDs = array();
 
-	foreach($oldxml->entry as $entry) {
-		if ($entry->link->attributes()->href) {
-			$urlstring = (string)$entry->link->attributes()->href;
-			if (preg_match("/twitter.com\/[A-Z0-9_]+\/status\/([0-9]+)/i", $urlstring, $matches)) { 
-				array_push($oldIDs, $matches[1]);
-			}
-		}
-	}
-	return $oldIDs;
+  foreach($oldxml->entry as $entry) {
+    if ($entry->link->attributes()->href) {
+      $urlstring = (string)$entry->link->attributes()->href;
+      if (preg_match("/twitter.com\/[A-Z0-9_]+\/status\/([0-9]+)/i", $urlstring, $matches)) { 
+        array_push($oldIDs, $matches[1]);
+      }
+    }
+  }
+  return $oldIDs;
 }
 
 // This function looks for new tweets and retweets them out
 function retweet() {
 
-	global $consumer_key, $consumer_secret, $access_token, $access_token_secret, $feed_url, $cached_file_path, $excludeIDs;
+  global $consumer_key, $consumer_secret, $access_token, $access_token_secret, $feed_urls, $cached_file_path, $excludeIDs;
+  foreach($feed_urls as $feed_url) {
+    $toa = new TwitterOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
 
-	$toa = new TwitterOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
+    $ch = curl_init($feed_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $feedcontents = curl_exec($ch);
+    curl_close($ch);
 
-	$ch = curl_init($feed_url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	$feedcontents = curl_exec($ch);
-	curl_close($ch);
+    $oldIDArray = getOldIDs();
 
-	$oldIDArray = getOldIDs();
-
-	$newxml = simplexml_load_string($feedcontents);
-  if (isset($newxml->entry)) {
-	  foreach($newxml->entry as $entry) {
-	    if ($entry->link->attributes()->href) {
-        $isreply = preg_match("/^@/i", (string)$entry->title); 
-        $urlstring = (string)$entry->link->attributes()->href;
-        $urlstringparsed = parse_url($urlstring);
-        $urlstringparsedpath = $urlstringparsed['path'];
-        $urlstringparsedpatharray = explode('/', $urlstringparsedpath);
-        $twitter_handle = $urlstringparsedpatharray[1];
-        $tweet_id = $urlstringparsedpatharray[2];
-        if (!isset($isreply)) {
+    $newxml = simplexml_load_string($feedcontents);
+    if (isset($newxml->entry)) {
+      foreach($newxml->entry as $entry) {
+        if ($entry->link->attributes()->href) {
+          $isreply = preg_match("/^@/i", (string)$entry->title); 
+          $urlstring = (string)$entry->link->attributes()->href;
+          $urlstringparsed = parse_url($urlstring);
+          $urlstringparsedpath = $urlstringparsed['path'];
+          $urlstringparsedpatharray = explode('/', $urlstringparsedpath);
+          $twitter_handle = $urlstringparsedpatharray[1];
+          $tweet_id = $urlstringparsedpatharray[2];
           if (preg_match("/twitter.com\/[A-Z0-9_]+\/status\/([0-9]+)/i", $urlstring, $matches)) {
             if (!in_array($matches[1], $oldIDArray)) {
               if (!in_array($twitter_handle, $excludeIDs)) {
-                print_r($toa->post('statuses/retweet/'.$matches[1]));
+                print "Retweet!\n";
+                $rt = $toa->post('statuses/retweet/'.$matches[1]);
               }
             }
           }
@@ -68,12 +68,12 @@ function retweet() {
             print_r($toa->post('statuses/update/', array('status' => $urlstring)));
           }
         }
-      }
-    }		
+      }    
+    }
   }
-	$handle = fopen($cached_file_path, 'w');
-	fwrite($handle,$feedcontents);
-	fclose($handle);	
+  $handle = fopen($cached_file_path, 'w');
+  fwrite($handle,$feedcontents);
+  fclose($handle);  
 }
 
 retweet();
